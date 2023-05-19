111bc07d0f0018d77365a43c5bbdb566f5c51806
kastastin <kostya242421@gmail.com>
2023-05-19 14:52:37 +0300

    Heroku Deploy

--- server.js
+++ server.js
@@ -15,4 +15,13 @@ app.use("/api/users", usersRoute);
 app.use("/api/buses", busesRoute);
 app.use("/api/seats", seatsRoute);
 
+const path = require("path");
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
+  });
+}
+
 app.listen(port, () => console.log(`Server is working on port ${port}`));
